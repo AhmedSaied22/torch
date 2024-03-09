e5a6397a8c34d150a4a23f3af8b6b84306d346cc
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2024-03-09 14:51:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TorchApp());
+}
+
+class TorchApp extends StatelessWidget {
+  const TorchApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
